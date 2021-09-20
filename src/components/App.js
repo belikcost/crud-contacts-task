5628c39ab85b6eceedc14206113a04902439b01c
belik9@inbox.ru <belik9@inbox.ru>
2021-09-20 18:37:15 +0300

    Separate auth and contacts by pages, small improve styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { Link, Redirect, Route, Switch } from "react-router-dom";
 import { Button, Form, ListGroup, Spinner } from "react-bootstrap";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,39 +14,55 @@ export const App = ({auth, handleAuthorizationRequest, contacts}) => {
 
     return (
         <div className="app app_background">
-            {auth ? (
-                <div className="app_modal">
-                    <h3>Контакты</h3>
-                    {contacts ? contacts.length !== 0 ? (
-                        <ListGroup variant="flush">
-                            {contacts.map(contact => (
-                                <ListGroup.Item key={contact.id}>
-                                    <h5>{contact.title}</h5>
-                                    <p>{contact.phone}</p>
-                                </ListGroup.Item>
-                            ))}
-                        </ListGroup>
-                    ) : (
-                        <p>Список контактов пуст</p>
-                    ) : (
-                        <Spinner animation="border" variant="primary" />
-                    )}
-                </div>
-            ) : (
-                <Form className="app_modal auth" onSubmit={onSubmit}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Логин</Form.Label>
-                        <Form.Control type="text" placeholder="Введите логин" required/>
-                    </Form.Group>
+            <header>
+                <Link to="/contacts">Контакты</Link>
+            </header>
+            <div className="app__content">
+                <Switch>
+                    <Route exact path="/">
+                        {auth ? (
+                            <Redirect to="/contacts"/>
+                        ) : (
+                            <Form className="app_modal auth" onSubmit={onSubmit}>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Логин</Form.Label>
+                                    <Form.Control type="text" placeholder="Введите логин" required/>
+                                </Form.Group>
 
-                    <Form.Group className="mb-3">
-                        <Form.Label>Пароль</Form.Label>
-                        <Form.Control type="password" placeholder="Введите пароль" required/>
-                    </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Пароль</Form.Label>
+                                    <Form.Control type="password" placeholder="Введите пароль" required/>
+                                </Form.Group>
 
-                    <Button variant="primary" type="submit" className="app_background">Войти</Button>
-                </Form>
-            )}
+                                <Button variant="primary" type="submit" className="app_background">Войти</Button>
+                            </Form>
+                        )}
+                    </Route>
+                    <Route path="/contacts">
+                        {auth ? (
+                            <div className="app_modal">
+                                <h3>Контакты</h3>
+                                {contacts ? contacts.length !== 0 ? (
+                                    <ListGroup variant="flush">
+                                        {contacts.map(contact => (
+                                            <ListGroup.Item key={contact.id}>
+                                                <h5>{contact.title}</h5>
+                                                <p>{contact.phone}</p>
+                                            </ListGroup.Item>
+                                        ))}
+                                    </ListGroup>
+                                ) : (
+                                    <p>Список контактов пуст</p>
+                                ) : (
+                                    <Spinner animation="border" variant="primary" />
+                                )}
+                            </div>
+                        ) : (
+                            <Redirect to="/"/>
+                        )}
+                    </Route>
+                </Switch>
+            </div>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import store from "./redux/store";
@@ -8,9 +9,11 @@ import AppContainer from "./containers/AppContainer";
 
 
 ReactDOM.render(
-    <Provider store={store}>
-        <AppContainer/>
-    </Provider>,
+    <Router>
+        <Provider store={store}>
+            <AppContainer/>
+        </Provider>
+    </Router>,
     document.getElementById('root')
 );
 
